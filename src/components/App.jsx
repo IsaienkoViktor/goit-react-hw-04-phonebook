7c626641ea440366cd5c76dfa4ea25bd35061f70
IsaienkoViktor <isaenkov1986@gmail.com>
2023-07-02 18:41:11 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactFilter } from './ContactFilter/ContactFilter';
 import { ContactList } from './ContactList/ContactList';
